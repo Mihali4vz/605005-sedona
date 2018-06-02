f74bbdcf0187d16a5c7f8f5c42b8d53833a08dcb
Михаил <lesnoy4ert@mail.ru>
2018-06-02 20:24:07 +0300

    внес исправления

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -1,18 +1,18 @@
-var togglenav = document.querySelector(".main-nav__toggle");
+var togglenav = document.querySelector(".page-header__toggle");
 var nav = document.querySelector(".main-nav__list");
-var closenav = document.querySelector(".exit-button");
+var closenav = document.querySelector(".site-list__button");
 var logo = document.querySelector(".page-header__logo-image")
 closenav.addEventListener("click", function (evt) {
 evt.preventDefault();
-nav.classList.remove("main-nav--show");
+nav.classList.remove("main-nav__list--show");
 nav.classList.add("main-nav--hide");
 logo.classList.add("logo-up");
-togglenav.classList.remove("toggle-hide");
+togglenav.classList.remove("page-header__toggle--hide");
 });
 togglenav.addEventListener("click", function (evt) {
 evt.preventDefault();
-nav.classList.add("main-nav--show");
+nav.classList.add("main-nav__list--show");
 nav.classList.remove("main-nav--hide");
 logo.classList.remove("logo-up");
-togglenav.classList.add("toggle-hide");
+togglenav.classList.add("page-header__toggle--hide");
 });
