4b884ba0079bc405c4d0de8fbf77f1bd467059b1
Михаил <lesnoy4ert@mail.ru>
2018-06-06 14:02:48 +0300

    Внес исправления на странице фото

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -1,22 +1,22 @@
 var togglenav = document.querySelector(".page-header__toggle");
 var nav = document.querySelector(".main-nav__list");
 var closenav = document.querySelector(".main-nav__button");
-var logo = document.querySelector(".page-header__logo-image");
+var logo = document.querySelector(".main-nav__logo-image");
 nav.classList.add("main-nav--hide");
-logo.classList.add("page-header__logo-image--up");
+logo.classList.add("main-nav__logo-image--up");
 togglenav.classList.remove("page-header__toggle--hide");
 closenav.classList.add("main-nav__button--show");
 closenav.addEventListener("click", function (evt) {
   evt.preventDefault();
   nav.classList.remove("main-nav__list--show");
   nav.classList.add("main-nav--hide");
-  logo.classList.add("page-header__logo-image--up");
+  logo.classList.add("main-nav__logo-image--up");
   togglenav.classList.remove("page-header__toggle--hide");
 });
 togglenav.addEventListener("click", function (evt) {
   evt.preventDefault();
   nav.classList.add("main-nav__list--show");
   nav.classList.remove("main-nav--hide");
-  logo.classList.remove("page-header__logo-image--up");
+  logo.classList.remove("main-nav__logo-image--up");
   togglenav.classList.add("page-header__toggle--hide");
 });
